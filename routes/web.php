87ab5554fa6d0e28dd2445792992e9cedfe3e55c
Lulu Khaira <lulukxhaira@gmail.com>
2026-03-15 22:41:17 +0700

    test UI hijau -lulu

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/dashboard_admin', function () {
     return view('dashboard_admin');
 });
-
-Route::get('/', function () {
-    return view('welcome');
-});
